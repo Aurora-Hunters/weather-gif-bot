478c8446890dda2228eaf0fb5e98f00eb443344d
Taly <vitalik7tv@yandex.ru>
2020-12-02 19:15:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,7 @@ bot.onText(/\/cme_lollipop/, (msg, match) => {
 
     const video = `https://iswa.gsfc.nasa.gov/IswaSystemWebApp/iSWACygnetStreamer?timestamp=2038-01-23+00%3A44%3A00&window=-1&cygnetId=261&t=${Date.now()}`;
     bot.sendChatAction(chatId, 'upload_video');
-    bot.sendVideo(chatId, video, {
-        reply_to_message_id: msg.message_id
-    });
+    bot.sendVideo(chatId, video);
 });
 
 bot.onText(/\/cme_pics/, (msg, match) => {
@@ -62,9 +60,7 @@ bot.onText(/\/cme_pics/, (msg, match) => {
             type: 'photo',
             media: `https://sohowww.nascom.nasa.gov/data/realtime/c3/512/latest.jpg?${Date.now()}`
         },
-    ], {
-        reply_to_message_id: msg.message_id
-    });
+    ]);
 });
 
 bot.onText(/\/clouds_sat/, async (msg, match) => {
@@ -79,9 +75,7 @@ bot.onText(/\/clouds_sat/, async (msg, match) => {
 
     clearInterval(intervalObject);
     bot.sendChatAction(chatId, 'upload_video');
-    bot.sendVideo(chatId, gifPath, {
-        reply_to_message_id: msg.message_id
-    });
+    bot.sendVideo(chatId, gifPath);
 });
 
 bot.onText(/\/clouds_pre/, async (msg, match) => {
@@ -96,7 +90,5 @@ bot.onText(/\/clouds_pre/, async (msg, match) => {
 
     clearInterval(intervalObject);
     bot.sendChatAction(chatId, 'upload_video');
-    bot.sendVideo(chatId, gifPath, {
-        reply_to_message_id: msg.message_id
-    });
+    bot.sendVideo(chatId, gifPath);
 });
